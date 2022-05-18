77989683a8c7456f60e0889a1b7fa0961510dfc9
vladandrei <vlad.andrei@flip.ro>
2022-05-18 17:45:18 +0300

    random function

--- contracts/DappCampNFT.sol
+++ contracts/DappCampNFT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+import "hardhat/console.sol";
 
 contract DappCampNFT is ERC721Enumerable, Ownable {
     uint256 public MAX_MINTABLE_TOKENS = 5;
@@ -13,23 +13,76 @@ contract DappCampNFT is ERC721Enumerable, Ownable {
     constructor() ERC721("DappCamp NFT", "DCAMP") Ownable() {}
 
     string[] private collection = [
-        ""
+        '{ "name": "caca" "description": "clap clap", "image": "<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 100 100"><rect width="100" height="100" fill="blue" /></svg>" }',
+        '{ "name": "maca", "description": "duck duck", "image": "<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 100 100"><rect width="100" height="100" fill="red" /></svg>" }'
     ];
 
-    function random(string memory input) internal pure returns (uint256) {
-        
+    function random(string memory _input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encode(_input)));
     }
 
-    function pluck(uint256 tokenId, string memory keyPrefix, string[] memory sourceArray) internal pure returns (string memory) {
-
+    /**
+     * @dev Function to convert uint to string
+     * Taken from https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol
+     */
+    function uint2str(uint256 _i)
+        internal
+        pure
+        returns (string memory _uintAsString)
+    {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = bytes1(uint8(48 + (_i % 10)));
+            _i /= 10;
+        }
+        return string(bstr);
     }
 
-    function tokenURI(uint256 tokenId) override public view returns (string memory) {
+    // string(abi.encodePacked(keyPrefix, uint2str(tokenId)))
+    function pluck(
+        uint256 _tokenId,
+        string memory _keyPrefix,
+        string[] memory sourceArray
+    ) internal view returns (string memory) {
+        uint256 rand = random(
+            string(abi.encodePacked(_keyPrefix, Strings.toString(_tokenId)))
+        );
+        uint256 index = rand % uint256(sourceArray.length);
+        console.log("plucking...", index);
+        return sourceArray[index];
+    }
 
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        string memory test = pluck(
+            _tokenId,
+            "data:application/json;base64",
+            collection
+        );
+        console.log("ClaiminGGG token ", test);
+
+        return pluck(_tokenId, "data:application/json;base64", collection);
     }
 
     function claim(uint256 tokenId) public {
-        require(tokenId > 0 && tokenId < MAX_MINTABLE_TOKENS, "Token ID invalid");
+        require(
+            tokenId > 0 && tokenId < MAX_MINTABLE_TOKENS,
+            "Token ID invalid"
+        );
         _safeMint(_msgSender(), tokenId);
     }
 }
